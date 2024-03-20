e36e05639aa750c7ef8a6c9109ff7e58c6efaeae
Shield <7nguyennguyen3@gmail.com>
2024-03-20 11:29:06 -0700

    Build a styled link component

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,9 +1,8 @@
-import React from "react";
-import { Button, Table } from "@radix-ui/themes";
-import Link from "next/link";
 import prisma from "@/prisma/client";
-import IssueStatusBadge from "../component/IssueStatusBadge";
+import { Table } from "@radix-ui/themes";
 import delay from "delay";
+import IssueStatusBadge from "../component/IssueStatusBadge";
+import Link from "../component/Link";
 import IssueActions from "./IssueActions";
 
 const IssuesPage = async () => {
