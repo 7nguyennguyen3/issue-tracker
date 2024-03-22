58f77c257ce9a7834f7b03a79c5785183e5cb6d7
Shield <7nguyennguyen3@gmail.com>
2024-03-22 14:52:40 -0700

    Enhance the API to assign issues

--- app/validationSchema.ts
+++ app/validationSchema.ts
@@ -4,5 +4,25 @@ export const issueSchema = z.object({
   title: z.string().min(1, "Title must contain at least 1 character").max(255),
   description: z
     .string()
-    .min(1, "Description must contain at least 1 character"),
+    .min(1, "Description must contain at least 1 character")
+    .max(65535),
+});
+
+export const patchIssueSchema = z.object({
+  title: z
+    .string()
+    .min(1, "Title must contain at least 1 character")
+    .max(255)
+    .optional(),
+  description: z
+    .string()
+    .min(1, "Description must contain at least 1 character")
+    .max(65535)
+    .optional(),
+  assignedToUserId: z
+    .string()
+    .min(1, "AssignedToUserId is required")
+    .max(255)
+    .optional()
+    .nullable(),
 });
